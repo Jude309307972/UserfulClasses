b946605b80687ce41e544213060a420c2401e5cf
Jude309307972 <jude@judedemac-mini.local>
2016-09-14 11:25:56 +0800

    Initial Commit

---
+++ UserfulClasses/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UserfulClasses
+//
+//  Created by Jude on 16/9/14.
+//  Copyright © 2016年 Jude. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UserfulClasses/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UserfulClasses
+//
+//  Created by Jude on 16/9/14.
+//  Copyright © 2016年 Jude. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
